7fb1821a97b78e0f841db842f0b49b451ab10972
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2019-04-02 21:57:28 +0800

    1

---
+++ Imay/Imay.Biz/ChatLayer/ChatClient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imay.Biz.ChatLayer
+{
+    public abstract class ChatClient
+    {
+        #region 启动
+        public abstract void Start();
+        #endregion
+
+        #region 停止
+        public abstract void Stop();
+        #endregion
+
+        #region MyRegion
+
+        #endregion
+    }
+}
---
+++ Imay/Imay.Biz/ChatLayer/TcpChatClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imay.Biz.ChatLayer
+{
+    internal class TcpChatClient : ChatClient
+    {
+        public TcpChatClient()
+        {
+
+        }
+
+        public override void Start()
+        {
+
+        }
+
+        public override void Stop()
+        {
+
+        }
+    }
+}
