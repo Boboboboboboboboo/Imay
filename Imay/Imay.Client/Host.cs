29c2937bbc9bc433e9ae23bffdbbdb16c9567212
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2019-04-02 22:06:58 +0800

    1

---
+++ Imay/Imay.Client/Host.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imay.Client
+{
+    public class Host
+    {
+        static void Main()
+        {
+            var service = new ServiceCollection();
+
+
+
+
+        }
+
+
+        private void Start(IServiceCollection service)
+        {
+            var provider = service.BuildServiceProvider();
+
+
+        }
+    }
+}
